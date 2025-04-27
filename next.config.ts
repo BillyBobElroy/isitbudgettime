6975569c20843fbcf4e90a6f8530123596b9333f
BillyBobElroy <william.forrester@hotmail.com>
2025-04-26 23:19:16 -0400

    Update: Minor fixes (1)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig: import('next').NextConfig = {
+  reactStrictMode: true,
+  experimental: {},
+  output: "standalone", // ✅ IMPORTANT for Vercel Tailwind build
 };
 
 export default nextConfig;
