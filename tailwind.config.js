9bad26c53a972360539c6785a7541799efc3413b
BillyBobElroy <william.forrester@hotmail.com>
2025-04-26 22:58:43 -0400

    Update: Minor Updates (6)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@
 const config = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
